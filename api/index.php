c39c0d0e0e4807fd755a3180f3811ab10eea69aa
jeiyuseu@gmail.com <jeiyuseu@gmail.com>
2021-12-23 09:06:23 +0800

    add prod env curl-url

--- api/index.php
+++ api/index.php
@@ -43,8 +43,12 @@ if (!isset($p)) {
     echo 'Bad Request!';
     die;
 }
+//local env
 
-curl_setopt($ch, CURLOPT_URL, "http://localhost:4000/api/v1/export/{$data->slug}/{$data->codename}?p={$p}");
+// curl_setopt($ch, CURLOPT_URL, "http://localhost:4000/api/v1/export/{$data->slug}/{$data->codename}?p={$p}");
+
+//prod env
+curl_setopt($ch, CURLOPT_URL, "https://jbwebapps.ga/phpspreadsheet-gmims/api/v1/export/{$data->slug}/{$data->codename}?p={$p}");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
